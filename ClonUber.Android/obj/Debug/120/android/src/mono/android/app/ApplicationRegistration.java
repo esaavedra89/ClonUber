6b51d6bbd19e72f7548b382fa4b3727c4607fa08
esaavedra <esaavedra@neotecnologias.com>
2022-10-02 13:12:38 -0400

    First commit

--- ClonUber.Android/obj/Debug/120/android/src/mono/android/app/ApplicationRegistration.java
+++ ClonUber.Android/obj/Debug/120/android/src/mono/android/app/ApplicationRegistration.java
@@ -5,6 +5,7 @@ public class ApplicationRegistration {
 	public static void registerApplications ()
 	{
 				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("GoogleMapsApi, ClonUber.Android, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc64df172952e985c2a2.GoogleMapsApi.class, crc64df172952e985c2a2.GoogleMapsApi.__md_methods);
 		
 	}
 }
