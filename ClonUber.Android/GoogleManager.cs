d9c9bcadc85804636c21eb5ad488ea4d3a6f687d
esaavedra <esaavedra@neotecnologias.com>
2022-11-09 11:18:21 -0400

    pendientes

---
+++ ClonUber.Android/GoogleManager.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Gms.Auth.Api;
+using Android.Gms.Auth.Api.SignIn;
+using Android.Gms.Common;
+using Android.Gms.Common.Apis;
+using Android.Icu.Util;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using ClonUber.Droid;
+using Firebase.Auth;
+using ClonUber.Modelo;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(GoogleManager))]
+namespace ClonUber.Droid
+{
+ 
+    public class GoogleManager : Java.Lang.Object, IGoogleManager, GoogleApiClient.IConnectionCallbacks, GoogleApiClient.IOnConnectionFailedListener
+    {
+        public Action<GoogleUser, string> _onLoginComplete;
+        public static GoogleApiClient _googleApiClient { get; set; }
+        public static GoogleManager Instance { get; private set; }
+        Context _context;
+
+        public GoogleManager()
+        {
+            _context = global::Android.App.Application.Context;
+            Instance = this;
+        }
+
+        /// <summary>
+        /// Deserialize all the information from GoogleApi.
+        /// </summary>
+        /// <param name="onLoginComplete"></param>
+        public void LogIn(Action<GoogleUser, string> onLoginComplete)
+        {
+            GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DefaultSignIn)
+                                                             .RequestEmail()
+                                                             .Build();
+            _googleApiClient = new GoogleApiClient.Builder((_context).ApplicationContext)
+                .AddConnectionCallbacks(this)
+                .AddOnConnectionFailedListener(this)
+                .AddApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .AddScope(new Scope(Scopes.Profile))
+                .Build();
+
+            _onLoginComplete = onLoginComplete;
+            Intent signInIntent = Auth.GoogleSignInApi.GetSignInIntent(_googleApiClient);
+            ((MainActivity)Forms.Context).StartActivityForResult(signInIntent, 1);
+            _googleApiClient.Connect();
+        }
+
+        /// <summary>
+        /// LogOut from GoogleApi and clear cache.
+        /// </summary>
+        public void LogOut()
+        {
+            var gsoBuilder = new GoogleSignInOptions.Builder(GoogleSignInOptions.DefaultSignIn).RequestEmail();
+
+            GoogleSignIn.GetClient(_context, gsoBuilder.Build())?.SignOut();
+
+            _googleApiClient.Disconnect();
+
+        }
+
+        /// <summary>
+        /// Verify if the mail is OK and capture the information.
+        /// </summary>
+        /// <param name="result"></param>
+        public void OnAuthCompleted(GoogleSignInResult result)
+        {
+            if (result.IsSuccess)
+            {
+                GoogleSignInAccount accountt = result.SignInAccount;
+                _onLoginComplete?.Invoke(new GoogleUser()
+                {
+                    Name = accountt.DisplayName,
+                    Email = accountt.Email,
+                    Picture = new Uri((accountt.PhotoUrl != null ? $"{accountt.PhotoUrl}" : $"https://autisticdating.net/imgs/profile-placeholder.jpg"))
+                }, string.Empty);
+            }
+            else
+            {
+                _onLoginComplete?.Invoke(null, "An error occured!");
+            }
+        }
+
+        public void OnConnected(Bundle connectionHint)
+        {
+
+        }
+
+        public void OnConnectionSuspended(int cause)
+        {
+            _onLoginComplete?.Invoke(null, "Canceled!");
+        }
+
+        public void OnConnectionFailed(ConnectionResult result)
+        {
+            _onLoginComplete?.Invoke(null, result.ErrorMessage);
+        }
+    }
+}
---
+++ ClonUber.Android/GoogleMapsApi.cs
@@ -0,0 +1,34 @@
+﻿#if DEBUG
+using System;
+using Android.App;
+using Android.Runtime;
+using Plugin.CurrentActivity;
+using ClonUber.Conexiones;
+[Application(Debuggable = true)]
+#else
+using Android.App;
+using Android.Runtime;
+using Plugin.CurrentActivity;
+using System;
+[Application(Debuggable = false]
+#endif
+
+
+[MetaData("com.google.android.maps.v2.API_KEY",
+    Value =Constantes.GoogleMapsApiKey)]
+public class GoogleMapsApi:Application
+{
+    // JniHandleOwnership help to transfer the Key to entire app.
+    // base, we subscribe to base on main activity.
+    public GoogleMapsApi(IntPtr handle, JniHandleOwnership transer)
+        : base(handle, transer)
+	{
+
+	}
+
+    public override void OnCreate()
+    {
+        base.OnCreate();
+        CrossCurrentActivity.Current.Init(this);
+    }
+}
